34247b5eed1851c6fa5cb79c8e05abeba563d334
Alston Dias <alston.dias1423@gmail.com>
2023-02-25 15:32:16 +0530

    add routes for userss

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask  
 from Routes.slides import app_routes
 from Routes.chatgpt import chatgpt_routes
-from Routes.mongo import mongo_routes
+from Routes.user import mongo_routes
 from Routes.dalle import dalle_routes
+from Routes.meeting import meeting_routes
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
   
 app = Flask(__name__) #creating the Flask class object   
@@ -10,6 +11,7 @@ app.register_blueprint(app_routes)
 app.register_blueprint(chatgpt_routes)
 app.register_blueprint(mongo_routes)
 app.register_blueprint(dalle_routes)
+app.register_blueprint(meeting_routes)
 
 # Configure JWT settings
 app.config['JWT_SECRET_KEY'] = 'mysecretkey' # Replace with a secret key of your choice
