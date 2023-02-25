aaf81f8205bc87baa4f6299abfa0fd7bf60e9c70
unknown <ARPGL002149$@persistent.co.in>
2023-02-25 21:02:00 +0530

    Chatgpt.py file updated and extra files added

--- Routes/chatgpt.py
+++ Routes/chatgpt.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify, Blueprint
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask import Flask, request, jsonify, Blueprint,make_response
 from pymongo import MongoClient
 from bson.json_util import dumps
 import openai
 import os
+import re
 
 # Set up OpenAI API key
 openai.api_key = os.environ['OPENAI_KEY']
@@ -16,17 +16,29 @@ db_name = 'Semicolons'
 
 # Define API endpoint for ChatGPT integration
 @chatgpt_routes.route('/api/chat', methods=['POST'])
-@jwt_required()
 def chat():
-    # Get user input from POST request
     user_input = request.json['message']
-    question = request.json['question']
-    username = request.json['username']
+    ans={}
+    ans['summary']=summary(user_input)
+    ans['completed']=completed(user_input)
+    ans['inprogress']=inprogress(user_input)
+    ans['test_cases']=test_cases(user_input)
+    ans['sprints']=sprints(user_input)
+    ans['budget']=budget(user_input)
 
+    #print(ans['summary'])
+    res =make_response(jsonify(ans), 200)
+    return res
+
+#function for Summary
+def summary(user_input):
+    # Get user input from POST request
+    question = "Summarize the sprint"
+    
     # Define the parameters for the GPT-3 API request
     prompt = f"{user_input + question}"
     temperature = 0.2
-    max_tokens = 50
+    max_tokens = 1024
 
     # Call the OpenAI GPT-3 API
     response = openai.Completion.create(
@@ -41,18 +53,168 @@ def chat():
     )
 
     # Extract the AI response from the API response
-    ai_response = response.choices[0].text.strip()
-
-    # Connect to MongoDB
-    client = MongoClient(uri)
-    db = client[db_name]
-
-    #Storing data to the database
-    db.chatgpt_response.insert_one({
-        'username': username,
-        'question': user_input,
-        'answer': ai_response
-    })
+    summary = response.choices[0].text.strip()
 
     # Return the AI response to the user
-    return jsonify({'ai_response': ai_response})
+    return summary
+
+#function for completed
+def completed(user_input):
+    # Get user input from POST request
+    question = "name of completed tasks by each team member"
+    
+    # Define the parameters for the GPT-3 API request
+    prompt = f"{user_input + question}"
+    temperature = 0.2
+    max_tokens = 1024
+
+    # Call the OpenAI GPT-3 API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract the AI response from the API response
+    completed = response.choices[0].text.strip()
+    
+    comp_dict = {}
+    for task in re.findall(r'(\w+):\s*(.*)\s*\(JIRA-ticket:(\d+)\)', completed):
+        comp_dict[task[0]] = f"{task[1]} (JIRA-ticket:{task[2]})"
+    
+    return comp_dict
+    
+
+#function for inprogress
+def inprogress(user_input):
+    # Get user input from POST request
+    question = "name of tasks in progress by each team member"
+    
+    # Define the parameters for the GPT-3 API request
+    prompt = f"{user_input + question}"
+    temperature = 0.2
+    max_tokens = 1024
+
+    # Call the OpenAI GPT-3 API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract the AI response from the API response
+    inprogress = response.choices[0].text.strip()
+
+    inpro_dict = {}
+    for task in re.findall(r'(\w+):\s*(.*)\s*\(JIRA-ticket:(\d+)\)', inprogress):
+        inpro_dict[task[0]] = f"{task[1]} (JIRA-ticket:{task[2]})"
+    
+    return inpro_dict
+
+#function for test_cases
+def test_cases(user_input):
+    # Get user input from POST request
+    question = "what were the test cases,their status in pass or fail and assigned to which team member in the sprint?give the answer in key value pairs"
+    
+    # Define the parameters for the GPT-3 API request
+    prompt = f"{user_input + question}"
+    temperature = 0.2
+    max_tokens = 1024
+
+    # Call the OpenAI GPT-3 API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract the AI response from the API response
+    test_cases = response.choices[0].text.strip()
+
+    tasks_dict = {}
+    for task in re.findall(r'(\w+):\s*([\w\s,]+),\s*JIRA-ticket:(\d+),\s*Status:\s*(\w+),\s*Assigned to:\s*(\w+)', test_cases):
+        tasks_dict[task[0]] = {
+            'test_case_name': task[1],
+            'status': task[3],
+            'executed_by': task[4]
+        }
+    return test_cases
+
+#function for sprints
+def sprints(user_input):
+    # Get user input from POST request
+    question = "count of tasks done by each team member throughout the sprint?"
+    
+    # Define the parameters for the GPT-3 API request
+    prompt = f"{user_input + question}"
+    temperature = 0.2
+    max_tokens = 1024
+
+    # Call the OpenAI GPT-3 API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract the AI response from the API response
+    sprints = response.choices[0].text.strip()
+
+    spr={}
+
+    name=re.findall(r'(\w+):', sprints)
+    no_tasks=re.findall(r': (\w+)', sprints)
+    cnt=0
+    for i in name:
+        spr[i]=no_tasks[cnt]
+        cnt+=1
+
+    return spr
+
+#functions for budget
+def budget(user_input):
+    # Get user input from POST request
+    question = "give month and estimate? key value pair"
+    
+    # Define the parameters for the GPT-3 API request
+    prompt = f"{user_input + question}"
+    temperature = 0.2
+    max_tokens = 1024
+
+    # Call the OpenAI GPT-3 API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract the AI response from the API response
+    budget = response.choices[0].text.strip()
+
+    # Return the AI response to the user
+    return budget
--- Routes/dalle.py
+++ Routes/dalle.py
@@ -1,37 +1,37 @@
-from flask import Flask, request, jsonify, Blueprint
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import openai
-import requests
-import os
+# from flask import Flask, request, jsonify, Blueprint
+# from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+# import openai
+# import requests
+# import os
 
-dalle_routes = Blueprint('dalle_routes', __name__)
+# dalle_routes = Blueprint('dalle_routes', __name__)
 
-# Set up OpenAI API key
-openai.api_key = os.environ['OPENAI_KEY']
+# # Set up OpenAI API key
+# openai.api_key = os.environ['OPENAI_KEY']
 
-# Define API endpoint for ChatGPT integration
-@dalle_routes.route('/api/dalle', methods=['POST'])
-@jwt_required()
-def chat():
-    # Get user input from POST request
-    user_input = request.json['description']
+# # Define API endpoint for ChatGPT integration
+# @dalle_routes.route('/api/dalle', methods=['POST'])
+# @jwt_required()
+# def chat():
+#     # Get user input from POST request
+#     user_input = request.json['description']
 
-    # Call the OpenAI GPT-3 API
-    response = openai.Image.create(
-    prompt=f"{user_input}",
-    model="image-alpha-001",
-    size="1024x1024",
-    response_format="url"
-)
+#     # Call the OpenAI GPT-3 API
+#     response = openai.Image.create(
+#     prompt=f"{user_input}",
+#     model="image-alpha-001",
+#     size="1024x1024",
+#     response_format="url"
+# )
 
-    # Extract the AI response from the API response
-    image_url =  response['data'][0]['url']
+#     # Extract the AI response from the API response
+#     image_url =  response['data'][0]['url']
 
-    # Return the AI response to the user
-    response = requests.get(image_url)
-    if response.status_code:
-        fp = open('download1.png', 'wb')
-        fp.write(response.content)
-        fp.close()
+#     # Return the AI response to the user
+#     response = requests.get(image_url)
+#     if response.status_code:
+#         fp = open('download1.png', 'wb')
+#         fp.write(response.content)
+#         fp.close()
 
-    return jsonify({'image-url:': image_url})
+#     return jsonify({'image-url:': image_url})
